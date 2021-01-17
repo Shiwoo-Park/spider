eb307c6b46eb65f51534a5cc19e33bba1a7f22f3
silva.park <woozin23@gmail.com>
2021-01-17 18:06:12 +0900

    added siders

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # spider
 
 crawler for diverse data
+
+## Dependencies
+- pyenv
+- python 3.7.6
+- scrapy
+
+## What's inside
+
+- egloos blog crawler
+
+
+## How to use
+
+```shell
+cd sample_spider
+scrapy runspider quote.py -o quotes.jl
+```
--- blog_spider/egloos.py
+++ blog_spider/egloos.py
@@ -1,12 +1,18 @@
 import scrapy
 
+
 class EgloosBlogSpider(scrapy.Spider):
     name = 'egloos_blogspider'
     start_urls = ['https://oniondev.egloos.com']
 
-    def parse(self, response):
-        for title in response.css('.post-header>h2'):
-            yield {'title': title.css('a ::text').get()}
+    def parse(self, response, **kwargs):
+        for quote in response.css('div.post_view'):
+            yield {
+                'title': quote.css('h2.entry-title').get(),
+                'text': quote.css('div.hentry').get(),
+            }
 
-        for next_page in response.css('a.next-posts-link'):
+        next_page = response.css('li.next a::attr("href")').get()
+        if next_page is not None:
             yield response.follow(next_page, self.parse)
+
