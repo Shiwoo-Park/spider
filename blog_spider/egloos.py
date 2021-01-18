6bb12582ce6ab885d0d6da5bc9eb20778170b2c8
silva.park <silva.park@sendbird.com>
2021-01-18 23:17:40 +0900

    simple update

--- blog_spider/egloos.py
+++ blog_spider/egloos.py
@@ -1,27 +1,38 @@
+import logging
 from datetime import datetime
 
 import pytz
 import scrapy
 
-EGLOOS_ID = "oniondev"
-BLOG_URL = f"http://{EGLOOS_ID}.egloos.com"
 
 class EgloosBlogSpider(scrapy.Spider):
+    '''
+    <Usage>
+    scrapy runspider egloos.py -a user_id=oniondev -o egloos_oniondev.json
+    '''
     name = 'egloos_blogspider'
-    start_urls = ['http://oniondev.egloos.com']
+    blog_url = None
+    start_urls = []
+
+    def __init__(self, user_id=None, *args, **kwargs):
+        super().__init__()
+        self.blog_url = f"http://{user_id}.egloos.com"
+        self.start_urls = [self.blog_url]
 
     def parse(self, response, **kwargs):
-        # title_xpath_4th = '//*[@id="section_content"]/div/div[4]/div[1]/div/h2/a'
-        # content_xpath_4th = '//*[@id="section_content"]/div/div[4]/div[2]/div/div'
-        # created_xpath = '//*[@id="section_content"]/div/div[1]/div[1]/ul/li[2]/abbr'
-
         posts = response.xpath('//*[@id="section_content"]/div/div')
         for post in posts:
             title = "".join(post.xpath('./div[1]/div/h2/a/text()').extract())
             content = "\n".join(post.xpath('./div[2]/div').css('*::text').getall())
             created_str = post.xpath('./div[1]/ul/li[2]/abbr/text()').get()
-            created_dt = datetime.strptime(created_str,"%Y/%m/%d %H:%M").replace(tzinfo=pytz.timezone('Asia/Seoul'))
-            print("created_dt:", created_dt)
+            try:
+                created_dt = datetime.strptime(created_str, "%Y/%m/%d %H:%M").replace(
+                    second=0,
+                    tzinfo=pytz.timezone('Asia/Seoul')
+                )
+                created_str = created_dt.strftime("%Y-%m-%d %H:%M:%S")
+            except Exception:
+                logging.error(f"Date parse failed: {created_str}")
 
             if not title or not content:
                 continue
@@ -29,11 +40,10 @@ class EgloosBlogSpider(scrapy.Spider):
             yield {
                 'title': title,
                 'content': content,
-                'created': str(created_dt)
+                'created': created_str,
             }
 
-        # pages = response.xpath('//*[@id="section_content"]/div/div[6]/span/a')
-        # for page in pages:
-        #     relative_path = page.xpath('./@href').get()  # (ex) /page/3
-        #     yield response.follow(f"{BLOG_URL}{relative_path}", self.parse)
-
+        pages = response.xpath('//*[@id="section_content"]/div/div[6]/span/a')
+        for page in pages:
+            relative_path = page.xpath('./@href').get()  # (ex) /page/3
+            yield response.follow(f"{self.blog_url}{relative_path}", self.parse)
